2faab001d26103858d6d43bb367aeb04d1d48c84
Elizabeth <56259225+Lokkie7@users.noreply.github.com>
2024-05-27 13:00:29 +1000

    Update locals.tf

--- 03/src/locals.tf
+++ 03/src/locals.tf
@@ -1,8 +1,1 @@
-locals {
-  tags = {
-    one = "${yandex_compute_disk.empty-disk[0].id}",
-    two = "${yandex_compute_disk.empty-disk[1].id}",
-    three = "${yandex_compute_disk.empty-disk[2].id}"
-    }
-}
 
